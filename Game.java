3611309dac1b5ec1cf4ee43f321bdea3de6a25c5
randomgraphics <gfxrgb@gmail.com>
2024-05-11 10:32:59 -0700

    Add ability to run command of a item in the room

--- Game.java
+++ Game.java
@@ -6,6 +6,8 @@ class Actor {
     public String name;        // the short name of the item
     public String description; // the long description text print to console when the player looks at the item
 
+    public ArrayList<Actor> inventory = new ArrayList<Actor>();
+	
     public String getDescription() {
         return description;
     }
@@ -17,6 +19,11 @@ class Actor {
             System.out.println(String.format("%s does't not understand the command %s.", name, command));
         }
     }
+
+    public void listInventory() {
+	// TODO: print all items in the inventory.
+	System.out.println(...);
+    }
 }
 
 // Represents the player in the game
@@ -24,8 +31,47 @@ class Player extends Actor {
     public Room currentRoom;
 }
 
+class Container extents Actor {
+    public Container() {
+    	name = "...";
+	    description = "This is a container.";
+    }
+
+    public String getDescription() {
+        System.out.println(description + " It is opened. It contains the following items:");
+        listInventory();
+    }
+
+    public void processCommand(Player player, String command, String[] args) {
+    	if ("t".equals(command) || "take".equals(command)) {
+            // The take command has 1 arguments: item.
+                // It takes the item from current actor's inventory list, move it into the player's inventory.
+            // for example: "container.take key" will move the "key", if exist, from container to player.
+        if (args.length < 1) {
+           // print an error.
+           System.out.println("What do you want to take?");
+        } else {
+           String itemName = args[0];
+           Actor target = findInventory(itemName);
+           if (null == target) {
+               // print error message.
+               System.out.println("....");
+           } else {
+               removeItemFromInventory(target);
+               player.inventory.add(target);
+           }
+        // TODO: consider add put command to put items back to a container or a room.
+        } else {
+                super.processCommand(player, command, args);
+            }
+        }
+    }
+}
+
 // Represents a room in the game world
 class Room extends Actor {
+    boolean locked = false;
+    
     Room upExit = null;
     Room downExit = null;
     Room northExit = null;
@@ -50,6 +96,19 @@ class Room extends Actor {
             go(player, eastExit);
         } else if ("w".equals(command)) {
             go(player, westExit);
+	} else if ("search".equals(command)) {
+            // run the list inventory method defined in the parent class (Actor)
+	    listInventory(args);
+	// handle command in form of "target.action"
+	} else if (command.contains(".")) {
+	    String targetName = findSubstringBeforeDot(command); // retrieve the target.
+	    String actionName = findSubstringAfterDot(command);  // retrieve the action. 
+        Actor target = findInventory(targetName);
+	    if (null != target) {
+            // let the target to process the action.
+            target.processCommand(player, actionName, args);
+	    else {
+		    System.out.println("There's no " + targetName + " found in current room");
         } else {
             super.processCommand(player, command, args);
         }
@@ -58,11 +117,18 @@ class Room extends Actor {
     private void go(Player player, Room room) {
         if (room == null) {
             System.out.println("You can't go that way.");
-        } else {
+        } else if (!room.locked) {
             System.out.println("You entered the " + room.name);
             player.currentRoom = room;
             room.processCommand(player, "look", null);
-        }
+	} else if (hasRoomKey(player, room)) {
+	    room.lock = false; // mark the room as unlocked.
+            System.out.println("You unlocked the " + room.name);
+            player.currentRoom = room;
+            room.processCommand(player, "look", null);
+        } else {
+	    System.out.println(room.name + "is locked. May be you need a key?");
+	}
     }
 }
 // set up a new room called living room 
@@ -82,9 +148,16 @@ class BasementHallway extends Room {
 
 class storageroom extends Room{
     public storageroom(){
-	name = "storageroom";
+	name = "Storage Room";
 	description = "You are at the storageroom, there is a box with lock on it, and a closet, bsement hallway is at west side ";
     }
+}
+
+class StorageRoomKey extents Actor {
+	public StorageRoomKey() {
+		name = "Storage Room Key";
+		description = "...";
+	}
 }
 
 class staircase extends Room {
@@ -167,7 +240,8 @@ public class Game {
         // We'll setup a simple game level that contains 2 rooms, a living room and a bedroom.
         // the living room is on the east side, and the bedroom is on the west side.
         Room livingRoom = new LivingRoom();
-	 Room storageroom = new storageroom();
+	Room storageroom = new storageroom();
+	storageroom.locked = true; // locke the storage room. so player can't go in by default.
         Room bedroom = new bedroom();
         Room kitchen = new kitchen();
         Room hallway = new hallway();
@@ -192,6 +266,11 @@ storageroom.westExit = BasementHallway;
         bedroom.southExit = guestbedroom;
         guestbedroom.northExit = bedroom;
 
+	// Create a container with the storage room key in it. Then put the container into the guest betroom.
+	Container container = new Contrainer;
+	StorageRoomKey key = new StorageRoomKey();
+	container.inventory.add(key);
+	guestroom.inventory.add(container);
 
         // Returns the living room as the starting room for the player
         return livingRoom;
