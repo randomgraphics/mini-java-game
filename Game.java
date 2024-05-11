93bdfb032ff27d3104443ca55ab22839352acfaa
randomgraphics <gfxrgb@gmail.com>
2024-05-11 09:09:42 -0700

    changes from Chengcheng

--- Game.java
+++ Game.java
@@ -26,6 +26,8 @@ class Player extends Actor {
 
 // Represents a room in the game world
 class Room extends Actor {
+    Room upExit = null;
+    Room downExit = null;
     Room northExit = null;
     Room southExit = null;
     Room eastExit = null;
@@ -37,7 +39,14 @@ class Room extends Actor {
             go(player, northExit);
         } else if ("s".equals(command)) {
             go(player, southExit);
-        } else if ("e".equals(command)) {
+        } 
+          else if ("u".equals(command)){
+              go(player, upExit);
+          }
+          else if ("d". equals(command)){
+              go(player, downExit);
+          }
+          else if ("e".equals(command)) {
             go(player, eastExit);
         } else if ("w".equals(command)) {
             go(player, westExit);
@@ -58,21 +67,70 @@ class Room extends Actor {
         }
     }
 }
-
+// set up a new room called living room 
 class LivingRoom extends Room {
     public LivingRoom() {
         name = "Living Room";
-        description = "You are in the living room. There is a TV, a table and a couch in the room.";
+        description = "You are in the living room. There is a TV, a table and a couch in the room. Hallway is at your east side ";
     }
 }
 
-class Bedroom extends Room {
-    public Bedroom() {
-        name = "Bedroom";
-        description = "You are in the bedroom. There is a bed and a closet in the room.";
+class BasementHallway extends Room {
+    public BasementHallway(){
+        name = "Basement Hallway ";
+        description = " you are in the basement. On your east side there is a storage room, and your west side, there is a locker room";
     }
 }
 
+class storageroom extends Room{
+public storageroom(){
+name = "storageroom";
+description = "You are at the storageroom, there is a box with lock on it, and a closet, bsement hallway is at west side ";
+}
+}
+
+
+class staircase extends Room {
+    public staircase(){
+        name = "staircase";
+        description = " You are at the staircase, going down is the basement, going up is the main floor";
+    }
+}
+// set up a new room including the action things could do in the room 
+class bedroom extends Room {
+    public bedroom() {
+        name = "bedroom";
+        description = "You are in the bedroom. There is a bed and a closet in the room, at you south side, that's the guestbedroom. Bathroom is at your east";
+    }
+}
+
+class kitchen extends Room{
+    public kitchen(){
+        name = "kitchen";
+        description = " You are in the Kitchen. There is a toaster, a fridge in this room.";
+    }
+}
+
+class hallway extends Room {
+    public hallway(){
+        name = "hallway";
+        description = "You are at the hall way, livingroom is at your west side, bedroom in at you east side, kitchen in at your south side, the staircase is at your down side ";
+    }
+}
+
+class guestbedroom extends Room {
+    public guestbedroom(){
+        name = "guestbedroom";
+        description = " You are in the guestbedroom, there is a bed, and a closet";
+    }
+}
+
+class bathroom extends Room {
+    public bathroom(){
+        name = "bathroom";
+        description = " You are in the bathroom, there is a shower room, toilet. Bedroom is on your west side";
+    }
+}
 public class Game {
     public static void main(String[] args) {
         // setup the game level.
@@ -87,17 +145,17 @@ public class Game {
         boolean gameOver = false;
         while (!gameOver) {
             // Read the user input
-            String input = scanner.nextLine();
-            String[] parts = input.split(" ");
-            String commandName = parts[0];
+            String input = scanner.nextLine();// read user input 
+            String[] parts = input.split(" "); // break up user input using " "
+            String commandName = parts[0]; 
             String[] commandArgs = Arrays.copyOfRange(parts, 1, parts.length);
 
             // Process the user input
             if ("quit".equals(commandName)) {
-                System.out.println("Game Over!");
+                System.out.println("Game Over!"); // if user enter "quit" game over= true 
                 gameOver = true;
             } else {
-                player.currentRoom.processCommand(player, commandName, commandArgs);
+                player.currentRoom.processCommand(player, commandName, commandArgs); //need to use current room to process command 
             }
         }
     }
@@ -107,9 +165,31 @@ public class Game {
         // We'll setup a simple game level that contains 2 rooms, a living room and a bedroom.
         // the living room is on the east side, and the bedroom is on the west side.
         Room livingRoom = new LivingRoom();
-        Room bedroom = new Bedroom();
-        livingRoom.eastExit = bedroom;
-        bedroom.westExit = livingRoom;
+	 Room storageroom = new storageroom();
+        Room bedroom = new bedroom();
+        Room kitchen = new kitchen();
+        Room hallway = new hallway();
+        Room bathroom = new bathroom();
+        Room staircase = new staircase();
+        Room BasementHallway = new BasementHallway();
+        Room guestbedroom = new guestbedroom();
+        livingRoom.eastExit = hallway;
+        hallway.westExit = livingRoom;
+        hallway.eastExit = bedroom;
+        hallway.downExit = staircase;
+        staircase.upExit = hallway;
+        staircase.downExit = BasementHallway;
+        BasementHallway.upExit = staircase;
+BasementHallway.eastExit = storageroom;
+storageroom.westExit = BasementHallway;
+        hallway.southExit = kitchen;
+        kitchen.northExit = hallway;
+        bedroom.westExit = hallway;
+        bedroom.eastExit = bathroom;
+        bathroom.westExit = bedroom;
+        bedroom.southExit = guestbedroom;
+        guestbedroom.northExit = bedroom;
+
 
         // Returns the living room as the starting room for the player
         return livingRoom;
