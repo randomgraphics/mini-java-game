9a4284cbdb06b4c2653d06b88a4be7eaf0bddfc1
randomgraphics <gfxrgb@gmail.com>
2024-05-19 09:39:06 -0700

    format cleanup

--- Game.java
+++ Game.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 // Base class of everything in the game world
 class Actor {
-    public String name;        // the short name of the item
+    public String name; // the short name of the item
     public String description; // the long description text print to console when the player looks at the item
 
     public ArrayList<Actor> inventory = new ArrayList<Actor>();
-	
+
     public String getDescription() {
         return description + listInventory();
     }
@@ -52,25 +52,28 @@ class Actor {
     }
 
     public String findSubstringBeforeDot(String s) {
-        // 1. search all letters within the string to locate the position of the first ".".
-        //    example: for "abc.def", the search should return 3.
+        // 1. search all letters within the string to locate the position of the first
+        // ".".
+        // example: for "abc.def", the search should return 3.
         int dotPosition = -1;
-        for(int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             if ('.' == s.charAt(i)) {
                 dotPosition = i;
                 break;
             }
         }
-       
+
         // 2. if there's no dot in the string, then return the whole string:
-        //    example: for "abcd", return "abcd".
-        if (-1 == dotPosition) return s;
-        
+        // example: for "abcd", return "abcd".
+        if (-1 == dotPosition)
+            return s;
+
         // 3. for string like ".abcd", should return empty string "".
-        if (0 == dotPosition) return "";
-        
+        if (0 == dotPosition)
+            return "";
+
         // 4. the return a substring from the first letter to the letter before the dot.
-        //    example: for "abc.def", return substring [0, 2].
+        // example: for "abc.def", return substring [0, 2].
         return s.substring(0, dotPosition); // 2nd argument of substr() is the length of the substring.
     }
 
@@ -86,30 +89,32 @@ class Player extends Actor {
 
 class Container extends Actor {
     public Container() {
-    	name = "box";
-	    description = "This is a container.";
+        name = "box";
+        description = "This is a container.";
     }
 
     public void processCommand(Player player, String command, String[] args) {
-    	if ("t".equals(command) || "take".equals(command)) {
+        if ("t".equals(command) || "take".equals(command)) {
             // The take command has 1 arguments: item.
-                // It takes the item from current actor's inventory list, move it into the player's inventory.
-            // for example: "container.take key" will move the "key", if exist, from container to player.
+            // It takes the item from current actor's inventory list, move it into the
+            // player's inventory.
+            // for example: "container.take key" will move the "key", if exist, from
+            // container to player.
             if (args.length < 1) {
-             // print an error.
-               System.out.println("What do you want to take?");
+                // print an error.
+                System.out.println("What do you want to take?");
             } else {
-               String itemName = args[0];
-               Actor target = findInventory(itemName);
-               if (null == target) {
-                   // print error message.
-                   System.out.println(" you picked up this item");
-               } else {
-                   removeItemFromInventory(target);
-                   player.inventory.add(target);
-               }
+                String itemName = args[0];
+                Actor target = findInventory(itemName);
+                if (null == target) {
+                    // print error message.
+                    System.out.println(" you picked up this item");
+                } else {
+                    removeItemFromInventory(target);
+                    player.inventory.add(target);
+                }
             }
-        // TODO: consider add put command to put items back to a container or a room.
+            // TODO: consider add put command to put items back to a container or a room.
         } else {
             super.processCommand(player, command, args);
         }
@@ -118,20 +123,20 @@ class Container extends Actor {
 
 class Letter extends Actor {
     public void processCommand(Player player, String command, String[] args) {
-       if ("r".equals(command) || "read".equals(command)) {
-           // TODO: read the content of the letter. print it out.
-       } else if ("h".equals(command) || "help".equals(command)) {
-           // TODO: print available commands of the letter.
-       } else {
+        if ("r".equals(command) || "read".equals(command)) {
+            // TODO: read the content of the letter. print it out.
+        } else if ("h".equals(command) || "help".equals(command)) {
+            // TODO: print available commands of the letter.
+        } else {
             super.processCommand(player, command, args);
-       } 
+        }
     }
 }
 
 // Represents a room in the game world
 class Room extends Actor {
     boolean locked = false;
-    
+
     Room upExit = null;
     Room downExit = null;
     Room northExit = null;
@@ -145,32 +150,30 @@ class Room extends Actor {
             go(player, northExit);
         } else if ("s".equals(command)) {
             go(player, southExit);
-        } 
-          else if ("u".equals(command)){
-              go(player, upExit);
-          }
-          else if ("d". equals(command)){
-              go(player, downExit);
-          }
-          else if ("e".equals(command)) {
+        } else if ("u".equals(command)) {
+            go(player, upExit);
+        } else if ("d".equals(command)) {
+            go(player, downExit);
+        } else if ("e".equals(command)) {
             go(player, eastExit);
         } else if ("w".equals(command)) {
             go(player, westExit);
-	    } else if ("search".equals(command)) {
+        } else if ("search".equals(command)) {
             listInventory();
-    	// handle command in form of "target.action"
+            // handle command in form of "target.action"
         } else if ("h".equals(command) || "help".equals(command)) {
-            System.out.println("your goal is to find what has happened in this house, , s stands for going south, n stands for north, w, stands for going north, e stands for going eask, l means look around ");
-	    } else if (command.contains(".")) {
-    	    String targetName = findSubstringBeforeDot(command); // retrieve the target.
-    	    String actionName = findSubstringAfterDot(command);  // retrieve the action. 
+            System.out.println(
+                    "your goal is to find what has happened in this house, , s stands for going south, n stands for north, w, stands for going north, e stands for going eask, l means look around ");
+        } else if (command.contains(".")) {
+            String targetName = findSubstringBeforeDot(command); // retrieve the target.
+            String actionName = findSubstringAfterDot(command); // retrieve the action.
             Actor target = findInventory(targetName);
-	        if (null != target) {
-            // let the target to process the action.
-            target.processCommand(player, actionName, args);
-	        } else {
-		       System.out.println("There's no " + targetName + " found in current room");
-            } 
+            if (null != target) {
+                // let the target to process the action.
+                target.processCommand(player, actionName, args);
+            } else {
+                System.out.println("There's no " + targetName + " found in current room");
+            }
         } else {
             super.processCommand(player, command, args);
         }
@@ -183,14 +186,14 @@ class Room extends Actor {
             System.out.println("You entered the " + room.name);
             player.currentRoom = room;
             room.processCommand(player, "look", null);
-	    } else if (room.hasKey(player)) {
-	        room.locked = false; // mark the room as unlocked.
+        } else if (room.hasKey(player)) {
+            room.locked = false; // mark the room as unlocked.
             System.out.println("You unlocked the " + room.name);
             player.currentRoom = room;
             room.processCommand(player, "look", null);
         } else {
-	    System.out.println(room.name + "is locked. May be you need a key?");
-	    }
+            System.out.println(room.name + "is locked. May be you need a key?");
+        }
     }
 
     // Check if the player has key of the room
@@ -200,7 +203,8 @@ class Room extends Actor {
         return false;
     }
 }
-// set up a new room called living room 
+
+// set up a new room called living room
 class LivingRoom extends Room {
     public LivingRoom() {
         name = "Living Room";
@@ -209,14 +213,14 @@ class LivingRoom extends Room {
 }
 
 class BasementHallway extends Room {
-    public BasementHallway(){
+    public BasementHallway() {
         name = "Basement Hallway ";
         description = " you are in the basement. On your east side there is a storage room, and your west side, there is a locker room";
     }
 }
 
-class storageroom extends Room{
-    public storageroom(){
+class storageroom extends Room {
+    public storageroom() {
         name = "Storage Room";
         description = "You are at the storageroom, there is a box with lock on it, and a closet, bsement hallway is at west side ";
     }
@@ -224,33 +228,36 @@ class storageroom extends Room{
     public boolean hasKey(Player player) {
         // check if the player has the key of the storage room.
         Actor key = player.findInventory("Storage Room Key");
-        if (null == key) return false; // no item named "Storage Room Key" in player's inventory, return false.
+        if (null == key)
+            return false; // no item named "Storage Room Key" in player's inventory, return false.
 
         // check if the key is an instance of StorageRoomKey.
         if (key instanceof StorageRoomKey) {
             // yes, this is indeed a StorageRoomKey. return true.
             return true;
         } else {
-            // no, this is something else just happen to be called "Storage Room Key". return false.
+            // no, this is something else just happen to be called "Storage Room Key".
+            // return false.
             return false;
         }
     }
 }
 
 class StorageRoomKey extends Actor {
-	public StorageRoomKey() {
-		name = "Storage Room Key";
-		description = " This key might help you finish the game";
-	}
+    public StorageRoomKey() {
+        name = "Storage Room Key";
+        description = " This key might help you finish the game";
+    }
 }
 
 class staircase extends Room {
-    public staircase(){
+    public staircase() {
         name = "staircase";
         description = " You are at the staircase, going down is the basement, going up is the main floor";
     }
 }
-// set up a new room including the action things could do in the room 
+
+// set up a new room including the action things could do in the room
 class bedroom extends Room {
     public bedroom() {
         name = "bedroom";
@@ -258,29 +265,29 @@ class bedroom extends Room {
     }
 }
 
-class kitchen extends Room{
-    public kitchen(){
+class kitchen extends Room {
+    public kitchen() {
         name = "kitchen";
         description = " You are in the Kitchen. There is a toaster, a fridge in this room.";
     }
 }
 
 class hallway extends Room {
-    public hallway(){
+    public hallway() {
         name = "hallway";
         description = "You are at the hall way, livingroom is at your west side, bedroom in at you east side, kitchen in at your south side, the staircase is at your down side ";
     }
 }
 
 class guestbedroom extends Room {
-    public guestbedroom(){
+    public guestbedroom() {
         name = "guestbedroom";
         description = " You are in the guestbedroom, there is a bed, and a closet";
     }
 }
 
 class bathroom extends Room {
-    public bathroom(){
+    public bathroom() {
         name = "bathroom";
         description = " You are in the bathroom, there is a shower room, toilet. Bedroom is on your west side";
     }
@@ -296,37 +303,40 @@ public class Game {
         // Create a scanner to read user input
         Scanner scanner = new Scanner(System.in);
 
-    	// Before game runs, print welcome screen to let player know how to play the game.
-    	printWelcome();
-	    
+        // Before game runs, print welcome screen to let player know how to play the
+        // game.
+        printWelcome();
+
         // Run the game loop
         boolean gameOver = false;
         while (!gameOver) {
             // Read the user input
-            String input = scanner.nextLine();// read user input 
+            String input = scanner.nextLine();// read user input
             String[] parts = input.split(" "); // break up user input using " "
-            String commandName = parts[0]; 
+            String commandName = parts[0];
             String[] commandArgs = Arrays.copyOfRange(parts, 1, parts.length);
-    
+
             // Process the user input
             if ("quit".equals(commandName)) {
-                    System.out.println("Game Over!"); // if user enter "quit" game over= true 
-                    gameOver = true;
+                System.out.println("Game Over!"); // if user enter "quit" game over= true
+                gameOver = true;
             } else if ("welcome".equals(commandName)) {
                 printWelcome();
             } else {
-                player.currentRoom.processCommand(player, commandName, commandArgs); //need to use current room to process command 
+                player.currentRoom.processCommand(player, commandName, commandArgs); // need to use current room to
+                                                                                     // process command
             }
         }
     }
 
     // Setup game level, return the first room that the player is in.
     static Room populateGameWorld() {
-        // We'll setup a simple game level that contains 2 rooms, a living room and a bedroom.
+        // We'll setup a simple game level that contains 2 rooms, a living room and a
+        // bedroom.
         // the living room is on the east side, and the bedroom is on the west side.
         Room livingRoom = new LivingRoom();
-    	Room storageroom = new storageroom();
-	    storageroom.locked = true; // locke the storage room. so player can't go in by default.
+        Room storageroom = new storageroom();
+        storageroom.locked = true; // locke the storage room. so player can't go in by default.
         Room bedroom = new bedroom();
         Room kitchen = new kitchen();
         Room hallway = new hallway();
@@ -341,9 +351,9 @@ public class Game {
         staircase.upExit = hallway;
         staircase.downExit = BasementHallway;
         BasementHallway.upExit = staircase;
-	    BasementHallway.eastExit = storageroom;
-	    storageroom.westExit = BasementHallway;        
-	    hallway.southExit = kitchen;
+        BasementHallway.eastExit = storageroom;
+        storageroom.westExit = BasementHallway;
+        hallway.southExit = kitchen;
         kitchen.northExit = hallway;
         bedroom.westExit = hallway;
         bedroom.eastExit = bathroom;
@@ -351,22 +361,23 @@ public class Game {
         bedroom.southExit = guestbedroom;
         guestbedroom.northExit = bedroom;
 
-    	// Create a container with the storage room key in it. Then put the container into the guest bedroom.
-    	Container container = new Container();
-    	StorageRoomKey key = new StorageRoomKey();
-    	container.inventory.add(key);
-    	guestbedroom.inventory.add(container);
+        // Create a container with the storage room key in it. Then put the container
+        // into the guest bedroom.
+        Container container = new Container();
+        StorageRoomKey key = new StorageRoomKey();
+        container.inventory.add(key);
+        guestbedroom.inventory.add(container);
 
         // Returns the living room as the starting room for the player
         return livingRoom;
     }
 
     static void printWelcome() {
-	// print welcome screen and simple game tutorial.
-	System.out.println("s, stands for going for south, n, stands for going north");
-	System.out.println(" e, stands for going east, w, stands for going west ");
-	System.out.println("Your goal is to find out what has happened in this house");
-	System.out.println(" print h for help ");
-	System.out.println("You can print this screen at any time during the game by typing \"help\" command");
+        // print welcome screen and simple game tutorial.
+        System.out.println("s, stands for going for south, n, stands for going north");
+        System.out.println(" e, stands for going east, w, stands for going west ");
+        System.out.println("Your goal is to find out what has happened in this house");
+        System.out.println(" print h for help ");
+        System.out.println("You can print this screen at any time during the game by typing \"help\" command");
     }
 }
